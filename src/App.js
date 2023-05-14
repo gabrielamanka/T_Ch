6e88e5b022210022a7fcf65b4c5ae5af31e40d7e
Gabriela <gabrielaamanka@gmail.com>
2023-05-14 20:16:53 +0200

    Dodano pliki

---
+++ src/App.js
@@ -0,0 +1,61 @@
+import "./App.css";
+
+import React, { useEffect, useState } from "react";
+
+function App() {
+  // utworzenie niezbednych stalych, dla adresu,strefy czasowej,bieżącej godziny
+  const [clientIP, setClientIp] = useState(null);
+  const [timezone, setTimezone] = useState(null);
+  const [currentTime, setCurrentTime] = useState(null);
+
+  useEffect(() => {
+    // pobranie z API adresu IP oraz strefy czasowej klienta
+    fetch("https://api.ipify.org/?format=json")
+      .then((response) => response.json())
+      .then((data) => {
+        setClientIp(data.ip);
+        setTimezone(Intl.DateTimeFormat().resolvedOptions().timeZone);
+      })
+      // w przypadku błędu zwróć błąd
+      .catch((error) => {
+        console.error("Błąd pobierania adresu IP klienta", error);
+      });
+  }, []);
+
+  // z kaźdą zmianą stanu strefy aktualizuj godzinę
+  useEffect(() => {
+    if (timezone) {
+      const intervalId = setInterval(() => {
+        // pobranie bieżącej godziny
+        setCurrentTime(
+          new Date().toLocaleTimeString("pl-PL", {
+            timeZone: timezone,
+          })
+        );
+      }, 1000); // aktualizuj co 1s
+      return () => clearInterval(intervalId);
+    }
+  }, [timezone]);
+
+  // sekcja logów
+  const PORT = process.env.PORT || 80;
+  const CURRENT_TIME = currentTime;
+  console.log(
+    `Autor: Gabriela Mańka` +
+      `Aplikacja nasłuchuje na porcie ${PORT}` +
+      `Start: ${CURRENT_TIME}`
+  );
+
+  // wyświetlenie wyników
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Adres IP: {clientIP}</p>
+        Strefa czasowa: {timezone}
+        <h1>{currentTime}</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
